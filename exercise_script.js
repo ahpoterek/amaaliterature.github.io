d5abe495e810242311dc9c2ff2e8e074009cc7eb
amaaliterature <amaaliterature@gmail.com>
2016-08-05 15:00:03 -0500

    Adding functions
    makes sure user answers every question, added functions to calculate the number of right and wrong answers, finds percentage correct

--- exercise_script.js
+++ exercise_script.js
@@ -1,14 +1,34 @@
-function handleClick()
+function Handleclick()
       {         
-var amountCorrect = 0;          
-for(var i = 1; i <= 45; i++) {
+var numRight = 0;
+var numWrong = 0;   
+var wrong=[];      
+for(var i = 1; i <= 9; i++) {
   var radios = document.getElementsByName('question_'+i);
   for(var j = 0; j < radios.length; j++) {
     var radio = radios[j];
     if(radio.value == "right" && radio.checked) {
-      amountCorrect++;
+      numRight++;
+    }
+    else if(radio.value=="wrong" && radio.checked){
+        numWrong++;
+        wrong.push(i)
     }
   }
-}                   
-        alert("Correct Responses: " + amountCorrect);
+}      
+        var percent= Math.round((numRight/9)*100);
+        if((numRight+numWrong)<9){
+            window.alert("Please answer every question");
+        }
+        else if((numRight+numWrong)==9){
+            alert("Correct Responses: " + numRight);
+            alert("Inorrect Responses: " + numWrong);
+            alert("Your answers are "+percent+" % correct");
+        for (i=0;i<wrong.length; i++){
+            alert("Question "+wrong[i]+" is incorrect");
+        }
+            
+        }
+        
+        
       }
