78ffa9b2c5b4c109be9ab456a4552b20dec1dc6f
amaaliterature <amaaliterature@gmail.com>
2016-08-05 13:46:51 -0500

    Changing functions
    Changed function to count the number of correct answers when button clicked (calling the function)

--- exercise_script.js
+++ exercise_script.js
@@ -1,8 +1,14 @@
-
-window.alert("Welcome to my website!");
-function select_wrong(){
-    window.alert("Oops, try again!");
-}
-function select_right(){
-    window.alert("Great job, that's exactly right! ");
-}
+function handleClick()
+      {         
+var amountCorrect = 0;          
+for(var i = 1; i <= 45; i++) {
+  var radios = document.getElementsByName('question_'+i);
+  for(var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if(radio.value == "right" && radio.checked) {
+      amountCorrect++;
+    }
+  }
+}                   
+        alert("Correct Responses: " + amountCorrect);
+      }
